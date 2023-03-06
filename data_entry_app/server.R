7949e29c514ad9d707b68fd330bf4c4df65cda49
kfavre <kiranfavre@yahoo.com>
2023-03-05 20:16:13 -0800

    added something i need to test still in the server.R file

--- data_entry_app/server.R
+++ data_entry_app/server.R
@@ -1,2 +1,11 @@
 #input and output will be lists of all defined inputs and outputs
-server <- function(input, output) {}
+server <- function(input, output){}#rest ofthis needs to be tested --, session) {
+ # observeEvent(input$switchtab, {
+  #  newtab <- switch(input$tabs,
+ #                    "dashboard" = "widgets",
+  #                   "widgets" = "dashboard"
+  #  )
+  #  updateTabItems(session, "tabs", newtab)
+  #})
+  
+#}
